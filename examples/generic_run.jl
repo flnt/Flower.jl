b4e586c648ebd5454af028a84decc5cbdab1c19a
flnt <tomas.fullana@upmc.fr>
2022-09-19 11:27:04 +0200

    changes in validation

--- examples/generic_run.jl
+++ examples/generic_run.jl
@@ -1,27 +1,84 @@
 using Revise
 using Flower
 
-fontsize_theme = Theme(fontsize = 30)
-set_theme!(fontsize_theme)
+# fontsize_theme = Theme(fontsize = 30)
+# set_theme!(fontsize_theme)
+
+# ratio = 1
+# L0 = 16.
+# nx = 128
+# ny = ratio * nx
+
+# x = LinRange(-L0/2, L0/2, nx+1)
+# y = LinRange(-ratio*L0/2, ratio*L0/2, ny+1)
+
+# num = Numerical(case = "Sphere",
+#     CFL = 0.1,
+#     TEND = 1.0,
+#     x = x,
+#     y = y,
+#     R = 1.56,
+#     save_every = 1,
+#     ϵ = 0.05,
+#     θd = 0.0,
+#     T_inf = -0.5
+# )
+
+# gp, gu, gv = init_meshes(num)
+# opS, opL, phS, phL, fwd = init_fields(num, gp, gu, gv)
+
+# @time MIXED, SOLID, LIQUID = run_forward(num, gp, gu, gv,
+#     opS, opL, phS, phL, fwd,
+#     stefan = true,
+#     advection = true,
+#     heat = true,
+#     heat_solid_phase = false,
+#     heat_liquid_phase = true,
+#     verbose = true,
+#     show_every = 1,
+# )
+
+# tcks = -ratio*L0/2:2:ratio*L0/2
+# lim = L0 / 2
+
+# fsolid = Figure(resolution = (1600, 1000))
+# colsize!(fsolid.layout, 1, Aspect(1, 1.0))
+# ax = Axis(fsolid[1,1], aspect = 1/ratio, xticks = tcks, yticks = tcks)  # customized as you see fit
+# heatmap!(gp.x[1,:], gp.y[:,1], phS.T')
+# contour!(gp.x[1,:], gp.y[:,1], gp.u', levels = 0:0, color=:red, linewidrth = 3);
+# # limits!(ax, -lim, lim, -lim, lim)
+# resize_to_layout!(fsolid)
+
+# # fsolid = current_figure()
+
+# fliquid = Figure(resolution = (1600, 1000))
+# colsize!(fliquid.layout, 1, Aspect(1, 1.0))
+# ax = Axis(fliquid[1,1], aspect = 1/ratio, xticks = tcks, yticks = tcks)  # customized as you see fit
+# heatmap!(gp.x[1,:], gp.y[:,1], phL.T')
+# contour!(gp.x[1,:], gp.y[:,1], gp.u', levels = 0:0, color=:red, linewidrth = 3);
+# contour!(gp.x[1,:], gp.y[:,1], fwd.usave[1,:,:]', levels = 0:0, color=:blue, linewidrth = 3);
+# # limits!(ax, -lim, lim, -lim, lim)
+# resize_to_layout!(fliquid)
+
+# # fliquid = current_figure()
 
 ratio = 1
-L0 = 16.
-nx = 128
+L0 = 2.
+nx = 16
 ny = ratio * nx
 
 x = LinRange(-L0/2, L0/2, nx+1)
 y = LinRange(-ratio*L0/2, ratio*L0/2, ny+1)
 
 num = Numerical(case = "Sphere",
-    CFL = 0.1,
-    TEND = 1.0,
+    CFL = 0.5,
+    TEND = 0.1,
     x = x,
     y = y,
-    R = 1.56,
+    R = 0.8,
     save_every = 1,
-    ϵ = 0.05,
-    θd = 0.0,
-    T_inf = -0.5
+    ϵ = 0.0,
+    θd = 1.0
 )
 
 gp, gu, gv = init_meshes(num)
@@ -29,12 +86,10 @@ opS, opL, phS, phL, fwd = init_fields(num, gp, gu, gv)
 
 @time MIXED, SOLID, LIQUID = run_forward(num, gp, gu, gv,
     opS, opL, phS, phL, fwd,
-    stefan = true,
-    advection = true,
     heat = true,
-    heat_solid_phase = false,
-    heat_liquid_phase = true,
-    verbose = true,
+    heat_solid_phase = true,
+    heat_liquid_phase = false,
+    verbose = false,
     show_every = 1,
 )
 
@@ -44,7 +99,7 @@ lim = L0 / 2
 fsolid = Figure(resolution = (1600, 1000))
 colsize!(fsolid.layout, 1, Aspect(1, 1.0))
 ax = Axis(fsolid[1,1], aspect = 1/ratio, xticks = tcks, yticks = tcks)  # customized as you see fit
-heatmap!(gp.x[1,:], gp.y[:,1], phS.T')
+heatmap!(gp.x[1,:], gp.y[:,1], phS.T', colorrange=(0, num.θd))
 contour!(gp.x[1,:], gp.y[:,1], gp.u', levels = 0:0, color=:red, linewidrth = 3);
 # limits!(ax, -lim, lim, -lim, lim)
 resize_to_layout!(fsolid)
@@ -54,9 +109,8 @@ resize_to_layout!(fsolid)
 fliquid = Figure(resolution = (1600, 1000))
 colsize!(fliquid.layout, 1, Aspect(1, 1.0))
 ax = Axis(fliquid[1,1], aspect = 1/ratio, xticks = tcks, yticks = tcks)  # customized as you see fit
-heatmap!(gp.x[1,:], gp.y[:,1], phL.T')
+heatmap!(gp.x[1,:], gp.y[:,1], phL.T', colorrange=(0, num.θd))
 contour!(gp.x[1,:], gp.y[:,1], gp.u', levels = 0:0, color=:red, linewidrth = 3);
-contour!(gp.x[1,:], gp.y[:,1], fwd.usave[1,:,:]', levels = 0:0, color=:blue, linewidrth = 3);
 # limits!(ax, -lim, lim, -lim, lim)
 resize_to_layout!(fliquid)
 
--- validation/cutcell_validation.jl
+++ validation/cutcell_validation.jl
@@ -1,12 +1,6 @@
 using Revise
 using Flower
 
-# using Revise
-# using Flower
-
-# fontsize_theme = Theme(fontsize = 30)
-# set_theme!(fontsize_theme)
-
 # ratio = 1
 # L0 = 2.
 # nx = 128
@@ -62,11 +56,11 @@ using Flower
 # # fliquid = current_figure()
 
 
-N_array = 2 .^[5, 6, 7]
+N_array = 2 .^[4, 5, 6, 7]
 plt = ("partial", "full", "all")
 plt2 = ("1", "2", "∞")
 
-function conv_cutcell_CN(x;
+function conv_cutcell_CN(arr;
     case = "Sphere",
     L0 = 2.,
     Θd = 1.0,
@@ -76,43 +70,54 @@ function conv_cutcell_CN(x;
     liquid = false,
     verbose = true)
 
-    N = length(x)
-    r = (x[end]+1)/(x[end-1]+1)
+    N = length(arr)
+    r = (arr[end]+1)/(arr[end-1]+1)
     ERR = zeros(3,3,N,2)
     T_data = Vector{Matrix{Float64}}(undef, 0)
     u_data = Vector{Matrix{Float64}}(undef, 0)
     grid = Vector{Vector{Float64}}(undef, 0)
 
     for i in 1:N
+        X = LinRange(-L0/2, L0/2, arr[i]+1)
+        Y = LinRange(-L0/2, L0/2, arr[i]+1)
         num = Numerical(case = case,
+            CFL = 0.5,
             L0 = L0,
             θd = Θd,
-            n = x[i] + 1,
+            save_every = 1,
+            x = X,
+            y = Y,
             max_iterations = ii*4^(i),
-            R = R + 1/x[1]);
-        idx, idxu, idxv = set_indices(num.n);
-        tmp, fwd = init_fields(num, idx, idxu, idxv);
-        MIXED, SOLID, LIQUID = run_forward(num, idx, idxu, idxv, tmp, fwd,
-        heat = true,
-        solid_phase = solid,
-        liquid_phase = liquid,
-        verbose = verbose
+            R = R,
+            ϵ = 0.0);
+
+        gp, gu, gv = init_meshes(num)
+        opS, opL, phS, phL, fwd = init_fields(num, gp, gu, gv)
+
+        @time MIXED, SOLID, LIQUID = run_forward(num, gp, gu, gv,
+            opS, opL, phS, phL, fwd,
+            heat = true,
+            heat_solid_phase = solid,
+            heat_liquid_phase = liquid,
+            verbose = verbose,
+            show_every = 1,
         )
+
         @show (num.max_iterations*num.τ)
         @show (num.τ)
         if solid
-            ERR[1, :, i, 1] .= normf(fwd.TS, MIXED, tmp.SOL[:,:,5], num.Δ)
-            ERR[2, :, i, 1] .= normf(fwd.TS, SOLID, tmp.SOL[:,:,5], num.Δ)
-            ERR[3, :, i, 1] .= normf(fwd.TS, vcat(SOLID, MIXED), tmp.SOL[:,:,5], num.Δ)
-            push!(T_data, fwd.TS)
+            ERR[1, :, i, 1] .= normf(phS.T, MIXED, gp.geoS.cap[:,:,5], num.Δ)
+            ERR[2, :, i, 1] .= normf(phS.T, SOLID, gp.geoS.cap[:,:,5], num.Δ)
+            ERR[3, :, i, 1] .= normf(phS.T, vcat(SOLID, MIXED), gp.geoS.cap[:,:,5], num.Δ)
+            push!(T_data, phS.T)
         elseif liquid
-            ERR[1, :, i, 1] .= normf(fwd.TL, MIXED, tmp.LIQ[:,:,5], num.Δ)
-            ERR[2, :, i, 1] .= normf(fwd.TL, LIQUID, tmp.LIQ[:,:,5], num.Δ)
-            ERR[3, :, i, 1] .= normf(fwd.TL, vcat(LIQUID, MIXED), tmp.LIQ[:,:,5], num.Δ)
-            push!(T_data, fwd.TL)
+            ERR[1, :, i, 1] .= normf(phL.T, MIXED, gp.geoL.cap[:,:,5], num.Δ)
+            ERR[2, :, i, 1] .= normf(phL.T, LIQUID, gp.geoL.cap[:,:,5], num.Δ)
+            ERR[3, :, i, 1] .= normf(phL.T, vcat(LIQUID, MIXED), gp.geoL.cap[:,:,5], num.Δ)
+            push!(T_data, phL.T)
         end
-        push!(u_data, fwd.u)
-        push!(grid, num.H)
+        push!(u_data, gp.u)
+        push!(grid, num.x)
     end
     try
         @assert N >= 3
@@ -130,8 +135,10 @@ end
 
 T_data, u_data, grid, ERR = conv_cutcell_CN(N_array,
 case = "Sphere",
-R = 0.75,
+R = 0.8,
 ii = 4,
+liquid = false,
+solid = true,
 verbose = false)
 
 
@@ -139,25 +146,36 @@ f = Figure(resolution = (1200, 1200))
 fontsize_theme = Theme(fontsize = 30)
 set_theme!(fontsize_theme)
 
-for i in 1:length(N_array)
+for i in 1:length(N_array)-1
     ax = Axis(f[1, i], title = "N = $(N_array[i])")
     colsize!(f.layout, i, Aspect(1, 1.0))
     hidedecorations!(ax)
     hm = heatmap!(f[1,i], grid[i], grid[i], T_data[i]', colormap = Reverse(:ice))
-    contour!(f[1,i], grid[i],grid[i],u_data[i]', levels = 0:0, linewidth = 4, color =:red)
+    #fm = contour!(f[1,i], grid[i], grid[i], u_data[i]', levels = 0:0, linewidth = 4, color =:red)
 end
 
 Colorbar(f[1, 4], vertical = true, flipaxis = true, label = "Temperature", ticks = 0:1, colormap = Reverse(:ice))
 ax = Axis(f[2:3,1:4], xscale = log2, yscale = log10,
 xticks = N_array, xlabel = "Points per dimension", ylabel = "Error")
 
-scatter!(f[2:3,1:4], N_array, ERR[1, 2, :, 2], label = "Partial cells, L-2 norm", markersize = 25, color =:black, marker=:rect)
-a, b = fit_order(N_array, ERR[1, 2, :, 2])
-lines!(f[2:3,1:4], N_array, a*(1 ./ N_array).^b, linewidth = 3, linestyle =:dash, color =:black, label = @sprintf "order %.2f" b)
+XX = abs.(ERR[1, 2, 1:end-1, 1] .- ERR[1, 2, end, 1])
 
-scatter!(f[2:3,1:4], N_array, ERR[2, 2, :, 2], label = "Full cells, L-2 norm", markersize = 25, color =:black, marker=:utriangle)
-a, b = fit_order(N_array, ERR[2, 2, :, 2])
-lines!(f[2:3,1:4], N_array, a*(1 ./ N_array).^b, linewidth = 3, linestyle =:dashdot, color =:black, label = @sprintf "order %.2f" b)
+scatter!(f[2:3,1:4], N_array[1:end-1], XX, label = "Mixed cells, L-2 norm", markersize = 25, color =:black, marker=:rect)
+a, b = fit_order(N_array[1:end-1], XX)
+lines!(f[2:3,1:4], N_array[1:end-1], a*(1 ./ N_array[1:end-1]).^b, linewidth = 3, linestyle =:dash, color =:black, label = @sprintf "order %.2f" b)
+
+YY = abs.(ERR[2, 2, 1:end-1, 1] .- ERR[2, 2, end, 1])
+
+scatter!(f[2:3,1:4], N_array[1:end-1], YY, label = "Full cells, L-2 norm", markersize = 25, color =:black, marker=:utriangle)
+a, b = fit_order(N_array[1:end-1], YY)
+lines!(f[2:3,1:4], N_array[1:end-1], a*(1 ./ N_array[1:end-1]).^b, linewidth = 3, linestyle =:dashdot, color =:black, label = @sprintf "order %.2f" b)
+
+ZZ = abs.(ERR[3, 2, 1:end-1, 1] .- ERR[3, 2, end, 1])
+
+scatter!(f[2:3,1:4], N_array[1:end-1], ZZ, label = "All cells, L-2 norm", markersize = 25, color =:black, marker=:triangle)
+a, b = fit_order(N_array[1:end-1], ZZ)
+lines!(f[2:3,1:4], N_array[1:end-1], a*(1 ./ N_array[1:end-1]).^b, linewidth = 3, linestyle =:dot, color =:black, label = @sprintf "order %.2f" b)
+
 
 axislegend(position = :lb)
 
