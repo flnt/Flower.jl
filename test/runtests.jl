468210dc4311948f1de047f127e39ccf7c2c5b3e
Paul102023 <paulr26@hotmail.fr>
2024-09-04 11:59:52 +0200

    test YAML file

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,28 +2,12 @@ using Flower
 using Test
 
 
-module testyamlfile
+module testyamlfile #enables to performa test with ARGS to give an input file
 ARGS = String["../examples/half_circle_imposed_radius.yml"]
 include("../examples/main_current_folder.jl")
 end
 @testset "Simple tests with YAML file" begin
-
-    # module testyamlfiles
-    # localARGS = String["../examples/half_circle_imposed_radius.yml"]
-    # ARGS = String["../examples/half_circle_imposed_radius.yml"]
-    # include("../examples/main_current_folder.jl")
-    # end
-    localARGS = String["../examples/half_circle_imposed_radius.yml"]
-    ARGS = String["../examples/half_circle_imposed_radius.yml"]
-    # localARGS = String["half_circle_imposed_radius.yml"]
-    # ARGS = String["half_circle_imposed_radius.yml"]
-    # print(localARGS)
-    # include("../examples/main_current_folder.jl")
-
-    # localARGS = String["../examples/half_circle_imposed_radius.yml"]; include("../examples/main_current_folder.jl")
-
     using testyamlfile
-
 end
 @testset "Simple tests" begin
     include("validation_test.jl")
